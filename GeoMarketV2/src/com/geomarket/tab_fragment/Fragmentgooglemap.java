030b2328d123734f6ce7974cd9c91cf9db57255b
fuhchang <fuhchang123@gmail.com>
2014-12-29 17:58:43 +0800

    updated

--- GeoMarketV2/src/com/geomarket/tab_fragment/Fragmentgooglemap.java
+++ GeoMarketV2/src/com/geomarket/tab_fragment/Fragmentgooglemap.java
@@ -1,12 +1,17 @@
 package com.geomarket.tab_fragment;
 
 import java.util.ArrayList;
+import java.util.Map;
 
 
 
 
 import java.util.List;
 
+import com.firebase.client.DataSnapshot;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.ValueEventListener;
 import com.geomarket.geofencing.GeofenceRequester;
 import com.geomarket.geofencing.SimpleGeofence;
 import com.geomarket.geofencing.SimpleGeofenceStore;
@@ -19,6 +24,7 @@ import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.GoogleMap.OnInfoWindowClickListener;
 import com.google.android.gms.maps.GoogleMap.OnMarkerClickListener;
 import com.google.android.gms.maps.GoogleMap.OnMarkerDragListener;
 import com.google.android.gms.maps.MapView;
@@ -45,14 +51,16 @@ import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
 import android.widget.Toast;
+import android.widget.FrameLayout.LayoutParams;
 
 public class Fragmentgooglemap extends Fragment  implements LocationListener,OnMarkerClickListener, OnMarkerDragListener, GooglePlayServicesClient.ConnectionCallbacks{
 	private MapView mMap;
 	private GoogleMap gMap;
 	private double lat = 1.3792457;
 	private double Lng = 103.85;
-
+	private Firebase ref = new Firebase("https://geomarket.firebaseio.com/location");
 	private int radius =500;
 	
 
@@ -60,7 +68,7 @@ public class Fragmentgooglemap extends Fragment  implements LocationListener,OnM
 		
 	}
 	@Override
-	public View onCreateView(LayoutInflater inflater,
+	public View onCreateView(final LayoutInflater inflater,
 			 ViewGroup container,  Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		View rootView = inflater.inflate(R.layout.activity_fragmentgooglemap, container, false);
@@ -72,9 +80,40 @@ public class Fragmentgooglemap extends Fragment  implements LocationListener,OnM
 		gMap = mMap.getMap();
 		gMap.getUiSettings().setMyLocationButtonEnabled(false);
 		gMap.setMyLocationEnabled(true);
-		gMap.addMarker(new MarkerOptions().position(new LatLng(lat, Lng)).title("nyp").snippet("testing123")).showInfoWindow();
-		CircleOptions co = new CircleOptions().center(new LatLng(lat, Lng)).radius(radius).fillColor(0x40ff0000).strokeColor(Color.TRANSPARENT).strokeWidth(2);
-		Circle circle = gMap.addCircle(co);
+		
+		ref.addValueEventListener(new ValueEventListener(){
+
+			@Override
+			public void onCancelled(FirebaseError firebaseError) {
+				// TODO Auto-generated method stub
+				Toast.makeText(getActivity(), "The read Fail " + firebaseError.getMessage() , Toast.LENGTH_LONG).show();
+			}
+
+			@Override
+			public void onDataChange(DataSnapshot snapshot) {
+				// TODO Auto-generated method stub
+				Map<String, Object> locsMap = (Map<String, Object>) snapshot.getValue();
+				for(String i : locsMap.keySet()){
+					Map<String, Object> locMap = (Map<String, Object>) locsMap.get(i);
+					
+					gMap.addMarker(new MarkerOptions().position(new LatLng(Double.parseDouble(locMap.get("lat").toString()), Double.parseDouble(locMap.get("lng").toString()))).title(i).snippet("Click here to view the offer!!")).showInfoWindow();
+					gMap.setOnInfoWindowClickListener(new OnInfoWindowClickListener(){
+
+						@Override
+						public void onInfoWindowClick(Marker marker) {
+							// TODO Auto-generated method stub
+							System.out.println(marker.getTitle());
+
+						}
+						
+					});
+					CircleOptions co = new CircleOptions().center(new LatLng(Double.parseDouble(locMap.get("lat").toString()), Double.parseDouble(locMap.get("lng").toString()))).radius(radius).fillColor(0x40ff0000).strokeColor(Color.TRANSPARENT).strokeWidth(2);
+					Circle circle = gMap.addCircle(co);
+				}
+			}
+			
+		});
+		
 		
 		
 		
